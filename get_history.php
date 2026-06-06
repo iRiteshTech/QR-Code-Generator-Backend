b84396fd7bee85668be4321ff989862bb81c62dd
iRiteshTech <riteshtiwary51@gmail.com>
2026-06-06 20:13:39 +0530

    Update get_history.php

--- get_history.php
+++ get_history.php
@@ -1,29 +1,8 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 include "db.php";
 
 $sql = "SELECT * FROM qr_history ORDER BY id DESC";
-
-$result = $conn->query($sql);
-
-$data = [];
-
-while($row = $result->fetch_assoc()){
-    $data[] = $row;
-}
-
-echo json_encode($data);
-
-?>
-<?php
-
-include "db.php";
-
-$sql = "SELECT * FROM qr_history ORDER BY id DESC";
-
 $result = $conn->query($sql);
 
 $data = [];
